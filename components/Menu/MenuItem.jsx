bb55f6c9d2d142e86e677b1d2893a96346eb185f
AYON <nafismahmudayon@gmail.com>
2024-09-22 23:54:56 +0600

    initial commit

--- components/Menu/MenuItem.jsx
+++ components/Menu/MenuItem.jsx
@@ -4,7 +4,7 @@ const MenuItem = ({ className, active, icon, iconStyle, title, onClick }) => {
   console.log(active, title)
 	return (
 		<div
-			className={`flex items-center gap-3 hover:bg-menuBackground cursor-pointer px-3 py-2 rounded-sm transition-all duration-200 ${className} ${active ? "bg-menuBackground" : ""} `}
+			className={`flex items-center gap-3 hover:bg-menuBackground cursor-pointer px-3 py-2 rounded-[4px] transition-all duration-200 ${className} ${active ? "bg-menuBackground" : ""} `}
 			onClick={onClick}>
 			<i className={`${iconStyle}`}>{icon}</i>
 			<span>{title}</span>
